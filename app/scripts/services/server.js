59658434df4e1c9f1fb7e4c8af3a9e732394eae8
seba-1511 <seba-1511@hotmail.com>
2015-02-12 05:42:02 +0000

    To beat Gabriel

--- app/scripts/services/server.js
+++ app/scripts/services/server.js
@@ -10,8 +10,10 @@
 angular.module('flusterFrontendApp')
     .service('Server', function() {
         //TODO change this url when we deploy on website
-        this.URL = 'http://localhost:9000/';
-        this.serverURL = 'http://localhost:8000/'
+        this.URL = 'https://getfluster.com/';
+        // this.URL = 'http://54.67.78.120/';
+        this.serverURL = 'https://54.67.78.120/api'
+        // this.serverURL = 'http://54.67.78.120:8000'
 
         this.authorizeURL =
             'https://www.dropbox.com/1/oauth2/authorize?response_type=token&client_id=bb6pz3atllflo3h&redirect_uri=' +
